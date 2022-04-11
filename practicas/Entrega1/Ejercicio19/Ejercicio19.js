2cd4723c2750ed4865814e80e186ccd5df674722
Agustina Rojas <89029960+agusrnfr@users.noreply.github.com>
2022-04-11 16:25:12 -0300

    Ejercicio 19

---
+++ practicas/Entrega1/Ejercicio19/Ejercicio19.js
@@ -0,0 +1,10 @@
+
+function valor(){
+    const dato = document.getElementById('seleccionar').value ;
+    const body = document.querySelector('body');
+    body.style.backgroundColor = dato;
+} 
+
+window.addEventListener('load', valor);
+
+
